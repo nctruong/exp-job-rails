e4dc7ad7563403654ba44b2d1a2c8ff48770b5c5
truong <willnguyen2971990@gmail.com>
2024-10-07 17:30:24 +0700

    refactor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,23 +6,36 @@ class Ability
   def initialize(user)
     user ||= AdminUser.new
 
-    if user.super_admin?
-      can :manage, :all
-    elsif user.admin?
-      user.permissions.for_admin.each do |permission|
-        if permission.condition_attribute.blank?
-          can permission.action.to_sym, permission.subject_class.constantize
-        else
-          can permission.action.to_sym, permission.subject_class.constantize, permission.condition_attribute.to_sym => user.send(permission.condition_value)
-        end
+    case user.role
+      when :super_admin
+        can :manage, :all
+      when :admin
+        verify_admin(user)
+      when :employer
+        verify_employer(user)
+    end
+  end
+
+  private
+
+  def verify_employer(user)
+    user.permissions.for_employer.each do |permission|
+      if permission.condition_attribute.blank?
+        can permission.action.to_sym, permission.subject_class.constantize
+      else
+        can permission.action.to_sym, permission.subject_class.constantize,
+            permission.condition_attribute.to_sym => user.send(permission.condition_value)
       end
-    elsif user.employer?
-      user.permissions.for_employer.each do |permission|
-        if permission.condition_attribute.blank?
-          can permission.action.to_sym, permission.subject_class.constantize
-        else
-          can permission.action.to_sym, permission.subject_class.constantize, permission.condition_attribute.to_sym => user.send(permission.condition_value)
-        end
+    end
+  end
+
+  def verify_admin(user)
+    user.permissions.for_admin.each do |permission|
+      if permission.condition_attribute.blank?
+        can permission.action.to_sym, permission.subject_class.constantize
+      else
+        can permission.action.to_sym, permission.subject_class.constantize,
+            permission.condition_attribute.to_sym => user.send(permission.condition_value)
       end
     end
   end
